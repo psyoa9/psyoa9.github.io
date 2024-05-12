201448e0b5ad5208b5ef0e400b4524e6579c1e40
psyoa9 <psyoa9@nottingham.ac.uk>
2024-05-12 14:29:05 +0100

    website

---
+++ script2.js
@@ -0,0 +1,24 @@
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://wmddzzuftfwnqnqpzcaq.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+
+
+export async function fetchData() {
+    const { data, error } = await supabase.from('People').select();
+    if (error) {
+        console.error('Error fetching data:', error.message);
+    } else {
+        console.log('Fetched data:', data);
+        // Do something with the fetched data, e.g., update the UI
+    }
+}
+
+
+
+document.querySelector('submit').addEventListener('click', fetchData);
+
+console.log("help");
+//const button = document.getElementById('submit');
+//button.addEventListener('click', fetchData);
